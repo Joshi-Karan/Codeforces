a0cbf457546f477566077e98b75a1e179ee987a1
Karan Joshi <karanforprogramming@gmail.com>
2021-12-28 23:15:47 +0530

    Create 112A.py

---
+++ 112A.py
@@ -0,0 +1,7 @@
+# Lucky Division
+
+val = int(input())
+if (val%4==0 or val%7==0 or val%47==0 or val%74==0 or val%44==0 or val%77==0 or val%444==0 or val%777==0 or val%447==0 or val%474==0 or val%477==0 or val%744==0 or val%747==0 or val%774==0):
+    print('YES')
+else:
+    print('NO')
